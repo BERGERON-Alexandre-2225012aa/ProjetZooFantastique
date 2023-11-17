3663198932801940991438d8db7e9f167997f6a9
AlexandreBergeronb22003371 <alexandrebergeron12@gmail.com>
2023-11-18 00:27:33 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ga2.projetzoofantastique {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ga2.projetzoofantastique to javafx.fxml;
+    exports ga2.projetzoofantastique;
+}
