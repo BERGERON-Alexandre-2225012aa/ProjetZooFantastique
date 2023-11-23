a6fbcf4abcf489dfc9b5444e8542d85b987dfead
Alexandre BERGERON <alexandrebergeron12@gmail.com>
2023-11-23 12:05:35 +0100

    Attributs Aquarium à terminer

---
+++ src/main/java/ga2/projetzoofantastique/models/Aquarium.java
@@ -0,0 +1,16 @@
+package ga2.projetzoofantastique.models;
+
+/**
+ * Classe Aquarium pour contenir des créatures aquatiques
+ */
+public class Aquarium extends Enclos {
+    //Attributs
+    /**
+     * Profondeur de l'aquarium
+     */
+    private double profondeur;
+    /**
+     * Indicateur de salinité de l'eau de l'aquarium
+     */
+    private int salinite;
+}
