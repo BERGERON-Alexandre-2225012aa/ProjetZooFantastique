0cffe4fab52077d41547d70ddd8da59620abddd7
ayoub <ayoub.bihi@etu.univ-amu.fr>
2023-11-25 17:58:47 +0100

    Ajout de la classe nymphe et de son test unitaire

---
+++ src/main/java/ga2/projetzoofantastique/models/creatures/vivipares/Nymphe.java
@@ -0,0 +1,46 @@
+package ga2.projetzoofantastique.models.creatures.vivipares;
+
+import ga2.projetzoofantastique.models.creatures.interfaces.Immortel;
+import ga2.projetzoofantastique.models.enclos.Enclos;
+
+import java.util.ArrayList;
+
+/**
+ * Classe Nymphe, sous classe de Vivipare qui implémente l'interface Immortel
+ */
+public class Nymphe extends Vivipare implements Immortel {
+    /**
+     * Constructeur de la classe Nymphe
+     * @param nom
+     * @param sexe
+     * @param poids
+     * @param taille
+     * @param aucun
+     */
+    public Nymphe(String nom, String sexe, double poids, double taille, Enclos aucun) {
+        super(nom, sexe, poids, taille, aucun);
+        this.tempsAvantNaissance = 9;
+        this.porteeMinimum = 1;
+        this.porteeMaximum = 3;
+        this.moisSaisonAmour = new ArrayList<>();
+        moisSaisonAmour.add(7);
+        moisSaisonAmour.add(8);
+        moisSaisonAmour.add(9);
+    }
+
+    /**
+     * Méthode pour mettre bas
+     */
+    @Override
+    public void mettreBas() {
+        //plus tard
+    }
+
+    /**
+     * Methode pour renaitre
+     */
+    @Override
+    public void renaitre() {
+        System.out.println(this.getNom() + " renait");
+    }
+}
---
+++ src/test/java/ga2/projetzoofantastique/models/creatures/vivipares/NympheTest.java
@@ -0,0 +1,60 @@
+package ga2.projetzoofantastique.models.creatures.vivipares;
+
+import ga2.projetzoofantastique.models.Maitre;
+import ga2.projetzoofantastique.models.ZooFantastique;
+import ga2.projetzoofantastique.models.enclos.Enclos;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Classe de tests pour la classe Nymphe
+ */
+class NympheTest {
+    /**
+     * Méthode de test pour le constructeur de la classe Nymphe
+     */
+    @Test
+    public void testConstructor(){
+        // On crée la nymphe.
+        Maitre maitre = new Maitre("Maitre1", "male");
+        ZooFantastique zooFantastique = new ZooFantastique("ZooFantastique1", maitre, 10);
+        Enclos aucun = new Enclos("Aucun enclos", 0, 0, zooFantastique);
+        Nymphe nymphe = new Nymphe("Nymphe1", "femelle", 50, 10, aucun );
+
+        // On crée les éléments auxquels les attributs de la nymphe doivent correspondre.
+        String nom = "Nymphe1";
+        String sexe = "femelle";
+        double poids = 50;
+        double taille = 10;
+        int age = 0;
+        int faim = 0;
+        boolean sommeil = false;
+        int sante = 2;
+        int tempsAvantNaissance = 9;
+        int porteeMinimum = 1;
+        int porteeMaximum = 3;
+        ArrayList<Integer> moisSaisonAmour = new ArrayList<>();
+        moisSaisonAmour.add(7);
+        moisSaisonAmour.add(8);
+        moisSaisonAmour.add(9);
+
+        // Les tests.
+        assertEquals(nom, nymphe.getNom());
+        assertEquals(sexe, nymphe.getSexe());
+        assertEquals(poids, nymphe.getPoids());
+        assertEquals(taille, nymphe.getTaille());
+        assertEquals(age, nymphe.getAge());
+        assertEquals(faim, nymphe.getFaim());
+        assertEquals(sommeil, nymphe.isSommeil());
+        assertEquals(sante, nymphe.getSante());
+        assertEquals(aucun, nymphe.getEnclos());
+        assertEquals(tempsAvantNaissance, nymphe.getTempsAvantNaissance());
+        assertEquals(porteeMinimum, nymphe.getPorteeMinimum());
+        assertEquals(porteeMaximum, nymphe.getPorteeMaximum());
+        assertEquals(moisSaisonAmour, nymphe.getMoisSaisonAmour());
+    }
+
+}
