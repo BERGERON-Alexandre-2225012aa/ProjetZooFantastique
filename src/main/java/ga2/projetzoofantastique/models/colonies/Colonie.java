61b569aabfc9ae3e9f2b38954cdedeca281bd956
GruZbob <alexandre.bergeron@etu.univ-amu.fr>
2023-11-27 18:12:44 +0100

    Colonie test doc attributs setters getters constructor

--- src/main/java/ga2/projetzoofantastique/models/colonies/Colonie.java
+++ src/main/java/ga2/projetzoofantastique/models/colonies/Colonie.java
@@ -2,9 +2,35 @@ package ga2.projetzoofantastique.models.colonies;
 
 import java.util.ArrayList;
 
+/**
+ * Classe Colonie
+ */
 public class Colonie {
+    /**
+     * Liste des meutes de la colonie
+     */
     private ArrayList<Meute> meutes;
+
+    /**
+     * Constructeur de la classe Colonie
+     */
     public Colonie() {
         this.meutes = new ArrayList<>();
+    }
+
+    /**
+     * Getter de liste des meutes de la colonie
+     * @return
+     */
+    public ArrayList<Meute> getMeutes() {
+        return meutes;
+    }
+
+    /**
+     * Setter de la liste des meutes de la colonie
+     * @param meutes
+     */
+    public void setMeutes(ArrayList<Meute> meutes) {
+        this.meutes = meutes;
     }
 }
---
+++ src/test/java/ga2/projetzoofantastique/models/colonies/ColonieTest.java
@@ -0,0 +1,58 @@
+package ga2.projetzoofantastique.models.colonies;
+
+import ga2.projetzoofantastique.models.Maitre;
+import ga2.projetzoofantastique.models.ZooFantastique;
+import ga2.projetzoofantastique.models.enclos.Enclos;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Classe de tests de la classe Colonie
+ */
+class ColonieTest {
+    @Test
+    public void testConstructor() {
+        // On crée la colonie.
+        Colonie colonie = new Colonie();
+
+        // On initialise les données souhaitées
+        ArrayList<Meute> meutes = new ArrayList<>();
+
+        //Test
+        assertEquals(meutes, colonie.getMeutes());
+    }
+
+    @Test
+    public void testGetters() {
+        // On crée la colonie.
+        Colonie colonie = new Colonie();
+
+        // On initialise les données souhaitées
+        ArrayList<Meute> meutes = new ArrayList<>();
+
+        //Test
+        assertEquals(meutes, colonie.getMeutes());
+    }
+
+    @Test
+    public void testSetters() {
+        // On crée la colonie.
+        Colonie colonie = new Colonie();
+
+        // On modifie les valeurs
+        Maitre maitre = new Maitre("Maitre1", "male");
+        ZooFantastique zooFantastique = new ZooFantastique("ZooFantastique1", maitre, 10);
+        Enclos enclos = new Enclos("Enclos1", 200, 20, zooFantastique);
+        Meute meute = new Meute(enclos);
+        ArrayList<Meute> meutes = new ArrayList<>();
+        meutes.add(meute);
+        colonie.setMeutes(meutes);
+
+        //Test
+        assertEquals(meutes, colonie.getMeutes());
+    }
+
+}
